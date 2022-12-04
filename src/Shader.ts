94f747b0f95ccfb38bbffbebb6514c2b3f644bd7
4nzi <zm17328@josai.ac.jp>
2022-12-04 12:35:10 +0900

    add shader parameter

--- src/Shader.ts
+++ src/Shader.ts
@@ -4,7 +4,11 @@ import Model from './Model'
 export class Shader {
   gl: WebGL2RenderingContext
   program: WebGLProgram
-  attLocation: { position: number }
+  attLocation: {
+    position: WebGLUniformLocation | null
+    normal: WebGLUniformLocation | null
+    uv: WebGLUniformLocation | null
+  }
   uniLocation: {
     mMatrix: WebGLUniformLocation | null
     vMatrix: WebGLUniformLocation | null
@@ -14,7 +18,6 @@ export class Shader {
   constructor(gl: WebGL2RenderingContext, vs: string, fs: string) {
     this.gl = gl
     this.program = glUtils(gl).createProgramShader(vs, fs)
-
     this.attLocation = glUtils(gl).getStandardAttLocations(this.program)
     this.uniLocation = glUtils(gl).getStandardUniLocations(this.program)
   }
@@ -38,10 +41,12 @@ export class Shader {
     this.gl.uniformMatrix4fv(this.uniLocation.mMatrix, false, mat)
     return this
   }
+
   setVmatrix(mat: number[]) {
     this.gl.uniformMatrix4fv(this.uniLocation.vMatrix, false, mat)
     return this
   }
+
   setPmatrix(mat: number[]) {
     this.gl.uniformMatrix4fv(this.uniLocation.pMatrix, false, mat)
     return this
@@ -64,13 +69,17 @@ export class Shader {
 }
 
 export class ToonShader extends Shader {
-  invLocation: WebGLUniformLocation | null
-  edgeLocation: WebGLUniformLocation | null
-  edgeWidthLocation: WebGLUniformLocation | null
-  lightLocation: WebGLUniformLocation | null
-  colorLocation: WebGLUniformLocation | null
-  albedoTexLocation: WebGLUniformLocation | null
-  normalTexLocation: WebGLUniformLocation | null
+  invMatrix: WebGLUniformLocation | null
+  lightPosition: WebGLUniformLocation | null
+  eyePosition: WebGLUniformLocation | null
+  color: WebGLUniformLocation | null
+  sdwThreshold: WebGLUniformLocation | null
+  hiThreshold: WebGLUniformLocation | null
+  rimThreshold: WebGLUniformLocation | null
+  edge: WebGLUniformLocation | null
+  edgeWidth: WebGLUniformLocation | null
+  albedoTex: WebGLUniformLocation | null
+  normalTex: WebGLUniformLocation | null
   texture: {
     albedo: WebGLTexture | null
     normal: WebGLTexture | null
@@ -78,15 +87,18 @@ export class ToonShader extends Shader {
 
   constructor(gl: WebGL2RenderingContext, vs: string, fs: string) {
     super(gl, vs, fs)
-
     //custom uniforms
-    this.invLocation = gl.getUniformLocation(this.program, 'invMatrix')
-    this.edgeLocation = gl.getUniformLocation(this.program, 'edge')
-    this.edgeWidthLocation = gl.getUniformLocation(this.program, 'edgeWidth')
-    this.lightLocation = gl.getUniformLocation(this.program, 'lightPosition')
-    this.colorLocation = gl.getUniformLocation(this.program, 'color')
-    this.albedoTexLocation = gl.getUniformLocation(this.program, 'albedoTex')
-    this.normalTexLocation = gl.getUniformLocation(this.program, 'normalTex')
+    this.invMatrix = gl.getUniformLocation(this.program, 'invMatrix')
+    this.lightPosition = gl.getUniformLocation(this.program, 'lightPosition')
+    this.eyePosition = gl.getUniformLocation(this.program, 'eyePosition')
+    this.sdwThreshold = gl.getUniformLocation(this.program, 'sdwThreshold')
+    this.hiThreshold = gl.getUniformLocation(this.program, 'hiThreshold')
+    this.rimThreshold = gl.getUniformLocation(this.program, 'rimThreshold')
+    this.color = gl.getUniformLocation(this.program, 'color')
+    this.albedoTex = gl.getUniformLocation(this.program, 'albedoTex')
+    this.normalTex = gl.getUniformLocation(this.program, 'normalTex')
+    this.edge = gl.getUniformLocation(this.program, 'edge')
+    this.edgeWidth = gl.getUniformLocation(this.program, 'edgeWidth')
 
     //store texture
     this.texture = {
@@ -96,22 +108,42 @@ export class ToonShader extends Shader {
   }
 
   setLightPosition(vec: number[]) {
-    this.gl.uniform3fv(this.lightLocation, vec)
+    this.gl.uniform3fv(this.lightPosition, vec)
+    return this
+  }
+
+  setEyePosition(vec: number[]) {
+    this.gl.uniform3fv(this.eyePosition, vec)
     return this
   }
 
   setInvMatrix(mat: number[]) {
-    this.gl.uniformMatrix4fv(this.invLocation, false, mat)
+    this.gl.uniformMatrix4fv(this.invMatrix, false, mat)
     return this
   }
 
   setColor(vec: number[]) {
-    this.gl.uniform3fv(this.colorLocation, vec)
+    this.gl.uniform3fv(this.color, vec)
     return this
   }
 
   setEdgeWidth(val: number) {
-    this.gl.uniform1f(this.edgeWidthLocation, val)
+    this.gl.uniform1f(this.edgeWidth, val)
+    return this
+  }
+
+  setSdwThreshold(val: number) {
+    this.gl.uniform1f(this.sdwThreshold, val)
+    return this
+  }
+
+  setHiThreshold(val: number) {
+    this.gl.uniform1f(this.hiThreshold, val)
+    return this
+  }
+
+  setRimThreshold(val: number) {
+    this.gl.uniform1f(this.rimThreshold, val)
     return this
   }
 
@@ -119,7 +151,6 @@ export class ToonShader extends Shader {
     await glUtils(this.gl)
       .loadTexture(src)
       .then((res) => (this.texture.albedo = res))
-
     return this
   }
 
@@ -129,13 +160,13 @@ export class ToonShader extends Shader {
     if (this.texture.albedo) {
       this.gl.activeTexture(this.gl.TEXTURE0)
       this.gl.bindTexture(this.gl.TEXTURE_2D, this.texture.albedo)
-      this.gl.uniform1i(this.albedoTexLocation, 0)
+      this.gl.uniform1i(this.albedoTex, 0)
     }
 
     if (this.texture.normal) {
       this.gl.activeTexture(this.gl.TEXTURE1)
       this.gl.bindTexture(this.gl.TEXTURE_2D, this.texture.normal)
-      this.gl.uniform1i(this.normalTexLocation, 1)
+      this.gl.uniform1i(this.normalTex, 1)
     }
 
     return this
@@ -151,12 +182,12 @@ export class ToonShader extends Shader {
 
     // model
     this.gl.cullFace(this.gl.BACK)
-    this.gl.uniform1i(this.edgeLocation, 0)
+    this.gl.uniform1i(this.edge, 0)
     this.gl.drawElements(this.gl.TRIANGLES, model.attribute.inx.length, this.gl.UNSIGNED_SHORT, 0)
 
     // edge
     this.gl.cullFace(this.gl.FRONT)
-    this.gl.uniform1i(this.edgeLocation, 1)
+    this.gl.uniform1i(this.edge, 1)
     this.gl.drawElements(this.gl.TRIANGLES, model.attribute.inx.length, this.gl.UNSIGNED_SHORT, 0)
 
     // unbind
